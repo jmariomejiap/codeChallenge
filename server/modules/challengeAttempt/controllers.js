008e278ea94eb7b0da3b87fba31ebb03ac8ff520
JM Mejia <jmariomejiap@gmail.com>
2017-06-27 16:38:33 -0700

    changing implementation after feedback

--- server/modules/challengeAttempt/controllers.js
+++ server/modules/challengeAttempt/controllers.js
@@ -1,21 +1,44 @@
 import ChallengeAttempt from '../../models/challenge_attempt';
+import jwt from 'jsonwebtoken';
+import * as config from '../../config';
 
-export function validateAccessCode(req, res, next) {
-  if (!req.body.accessCode || !req.body.passCode) {
+
+export function verifyAccess(req, res, next) {
+  const accessCode = req.body.accessCode;
+  const passCode = req.body.passCode;
+
+  if (!accessCode || !passCode) {
     return res.status(404).json({ result: 'error', error: 'missing_parameters' });
   }
-  return ChallengeAttempt.verifyAccess(req.body.accessCode, req.body.passCode, (err, dbData) => {
+  return ChallengeAttempt.find({ accessCode, passCode }, (err, data) => {
     if (err) {
       return res.status(500).json({ result: 'error', error: 'internal_error' });
     }
-    if (dbData === 'invalid') {
+    if (data.length === 0) {
       return res.status(404).json({ result: 'error', token: '', error: 'invalid_access_tokens' });
     }
-    if (dbData === 'completed') {
-      return res.status(404).json({ result: 'error', token: '', error: 'challenge_completed' });
+    req.dbData = data[0]; // eslint-disable-line no-param-reassign
+    return next();
+  });
+}
+
+export function verifyStatus(req, res, next) {
+  const status = req.dbData.status;
+  if (status === 'completed') {
+    return res.status(404).json({ result: 'error', token: '', error: 'challenge_completed' });
+  }
+  return next();
+}
+
+export function generateToken(req, res, next) {
+  const payLoad = { challengeAttemptId: req.dbData._id };
+  const options = {};
+  const key = config.default.secretKey;
+  jwt.sign(payLoad, key, options, (err, token) => {
+    if (err) {
+      return res.status(500).json({ result: 'error', error: 'internal_error' });
     }
-    // res.status(200).json({ result: 'ok', token: tokenValue, error: '' });  // if res not here error is thrown when trying to commit.
-    req.token = dbData; // eslint-disable-line no-param-reassign
+    req.token = token; // eslint-disable-line no-param-reassign
     return next();
   });
 }
--- server/modules/challengeAttempt/routes.js
+++ server/modules/challengeAttempt/routes.js
@@ -4,7 +4,11 @@ import * as Controller from './controllers';
 const router = new Router();
 
 // step one, validateAccessCode
-router.post('/accesscode', Controller.validateAccessCode);
+router.post('/accesscode/',
+  Controller.verifyAccess,
+  Controller.verifyStatus,
+  Controller.generateToken,
+);
 
 
 export default router;
