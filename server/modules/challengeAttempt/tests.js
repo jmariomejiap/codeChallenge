a9077c2be5e2b2808cf9d0a3597ee7eb04f77724
Oscar Mejia <oscmejia@vovsolutions.com>
2017-07-23 11:54:38 -0500

    wip

--- server/modules/challengeAttempt/tests.js
+++ server/modules/challengeAttempt/tests.js
@@ -14,7 +14,33 @@ test.cb.before('connecting to challenge', (t) => {
   });
 });
 
+export async function wait(sec) {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('continue');
+    }, (sec * 1000));
+  });
+};
 
 test('Should always pass', t => {
   t.is(true, true);
+});
+
+test.cb('Should wait for a timeout', t => {
+  setTimeout(() => {
+      t.is(true, true);
+      t.end();
+  }, (200));
+});
+
+test('Should wait for a promise', async (t) => {
+  console.log('begining of tests');
+  const res = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('*******');
+    }, (200));
+  });
+
+  t.is(true, true);
+  console.log('end of tests' + res);
 });
