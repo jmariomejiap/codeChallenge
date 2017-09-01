634da9482eb12bcbd49de13798136df405eb7faa
Oscar Mejia <oscmejia@vovsolutions.com>
2017-09-01 18:35:59 -0500

    Improve challengeStep model definition

--- server/models/challengeStep.js
+++ server/models/challengeStep.js
@@ -13,7 +13,10 @@ const mongoIdValidator = [
 const challengeStep = new Schema({
   id: { type: 'String', required: true },
   challengeId: { type: 'String', required: true, validate: mongoIdValidator },
-  description: { type: 'String', required: true },
+  challengeStepId: { type: 'String', required: true, validate: mongoIdValidator },
+  answer: { type: 'String' },
+  score: { type: 'Number', required: true },
+  dateFinished: { type: Date, default: Date.now },
 });
 
 export default mongoose.model('ChallengeStep', challengeStep);
