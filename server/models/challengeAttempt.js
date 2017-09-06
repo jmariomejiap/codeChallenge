a7d61b2dab8d18d062e18f44e6f68c0cd9970495
JM Mejia <jmariomejiap@gmail.com>
2017-09-05 22:26:39 -0700

    validator are working, magically

--- server/models/challengeAttempt.js
+++ server/models/challengeAttempt.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
-// import validate from 'mongoose-validator';
+import validate from 'mongoose-validator';
 
 const Schema = mongoose.Schema;
-/*
+
 const emailValidator = [
   validate({
     validator: 'isEmail',
@@ -17,29 +17,14 @@ const mongoIdValidator = [
   }),
 ];
 
-const mongoIdValidator = [
-  validate({
-    validator: function(val) {
-      return new Promise((resolve, reject) => {
-        if (isMongoId) {
-          resolve(true)
-        }
-        reject(false);
-      })
-    },
-    message: "error with mongodId schema",
-  }),
-];
-*/
-
 const challengeAttempt = new Schema({
   accessCode: { type: String, required: true },
   passCode: { type: String, required: true },
   fullName: { type: String, lowercase: true, required: true },
-  email: { type: String, lowercase: true, required: true },
+  email: { type: String, lowercase: true, required: true, validate: emailValidator },
   score: { type: Number, min: 0, max: 100 },
   currentStepId: { type: String, required: true },
-  challengeId: { type: String, required: true },
+  challengeId: { type: String, required: true, validate: mongoIdValidator },
   status: { type: String, required: true, enum: ['not_started', 'in_progress', 'completed'] },
 });
 
