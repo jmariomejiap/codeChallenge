254ea61599f985590106c44c60dc8627105b3239
Oscar Mejia <oscmejia@vovsolutions.com>
2017-09-22 21:45:07 -0500

    improve general test coverage

--- server/util/validateAccess.js
+++ server/util/validateAccess.js
@@ -1,6 +1,6 @@
 import supertest from 'supertest-as-promised'; // eslint-disable-line
 
-
+/* istanbul ignore next */
 const fetchToken = async (aCode, pCode) => {
   const res = await supertest('http://localhost:8080')
     .get(`/api/v1/challengeAttempt?accessCode=${aCode}&passCode=${pCode}`);
