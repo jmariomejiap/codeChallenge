d9627e3a49103734df7d40cb9122cc88dc074d79
Oscar Mejia <oscmejia@vovsolutions.com>
2017-09-22 20:19:07 -0500

    remove catch

--- server/modules/challengeStep/tests.js
+++ server/modules/challengeStep/tests.js
@@ -1,12 +1,12 @@
-import test from 'ava'; // eslint-disable-line 
-import supertest from 'supertest-as-promised'; // eslint-disable-line 
-import server from '../../server.js'; // eslint-disable-line 
+import test from 'ava'; // eslint-disable-line
+import supertest from 'supertest-as-promised'; // eslint-disable-line
+import server from '../../server.js'; // eslint-disable-line
 import Challenge from '../../models/challenge.js';
 import ChallengeAttempt from '../../models/challengeAttempt.js';
 import fetchToken from '../../util/validateAccess.js';
 
 const internals = {};
-internals.dummyDataToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGFsbGVuZ2VBdHRlbXB0SWQiOiI1OWFmODEwZjkwYzQ4NjNmYWE5YTAyYjkiLCJjaGFsbGVuZ2VJZCI6IjU5YWY4MTBlOTBjNDg2M2ZhYTlhMDJiOCIsInVzZXJGdWxsTmFtZSI6ImR1bW15dXNlcm5hbWUiLCJpYXQiOjE1MDUzMjIxMTd9.JtD7alaWlI_aUF0FCF8dxekTm1DYR1Z5NKQkIA8GZ1I'; // eslint-disable-line  
+internals.dummyDataToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGFsbGVuZ2VBdHRlbXB0SWQiOiI1OWFmODEwZjkwYzQ4NjNmYWE5YTAyYjkiLCJjaGFsbGVuZ2VJZCI6IjU5YWY4MTBlOTBjNDg2M2ZhYTlhMDJiOCIsInVzZXJGdWxsTmFtZSI6ImR1bW15dXNlcm5hbWUiLCJpYXQiOjE1MDUzMjIxMTd9.JtD7alaWlI_aUF0FCF8dxekTm1DYR1Z5NKQkIA8GZ1I'; // eslint-disable-line
 internals.emptyPayloadToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.OatqkwDnaqBNtgHHYJFGMioVx_9ZZ_sePRYyENAx5to';
 
 test.before('establish connection ', () => {
@@ -55,7 +55,7 @@ test('should not have access to dummyData', async (t) => {
 test('internal error if token Has been tampered', async (t) => {
   const token = await fetchToken('myAccessCodeTest', 'myPassCodeTest');
   const res = await internals.reqAgent
-    .get(`/api/v1/challenge?token=${token}X`); // eslint-disable-line  
+    .get(`/api/v1/challenge?token=${token}X`); // eslint-disable-line
 
   t.is(res.status, 500);
   t.is(res.body.result, 'error');
@@ -75,7 +75,7 @@ test('should not accept post with right arguments', async (t) => {
   const token = await fetchToken('myAccessCodeTest', 'myPassCodeTest');
   const res = await internals.reqAgent
     .post('/api/v1/challengeStep')
-    .send({ token }); // eslint-disable-line 
+    .send({ token }); // eslint-disable-line
 
   t.is(res.status, 404);
   t.falsy(res.body.result, 'result is non existent');
@@ -119,10 +119,8 @@ test('successful test, should return currentStep found in challengeAttempt colle
 
 
 test('should fail if challengeStep contentS are incomplete', async (t) => {
-  await ChallengeAttempt.update({ accessCode: 'myAccessCodeTest', passCode: 'myPassCodeTest' }, { currentStepId: '004' })
-    .catch((e) => {
-      return e;
-    });
+  await ChallengeAttempt.update({ accessCode: 'myAccessCodeTest', passCode: 'myPassCodeTest' }, { currentStepId: '004' });
+
   const token = await fetchToken('myAccessCodeTest', 'myPassCodeTest');
   const res = await internals.reqAgent
     .get(`/api/v1/challengeStep?token=${token}`);
