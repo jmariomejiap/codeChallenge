1f03b4a798788ea39b50722e3b30aa90d97e3c1e
jmario <jmariomejiap@gmail.com>
2017-11-20 22:14:07 -0800

    removing unused variables

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ import compression from 'compression';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import path from 'path';
-// import IntlWrapper from '../client/modules/Intl/IntlWrapper'; // eslint-disable-line no-unused-vars
 
 // Webpack Requirements
 import webpack from 'webpack';
@@ -12,18 +11,13 @@ import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 
 // React And Redux Setup
-// import { configureStore } from '../client/store'; // eslint-disable-line no-unused-vars
-// import { Provider } from 'react-redux'; // eslint-disable-line no-unused-vars
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { match, RouterContext } from 'react-router';
 import Helmet from 'react-helmet';
 
 // Import required modules
-// import routes from '../client/routes';
 import MyRoutes from '../client/NewRoutes';
-// import ErrorMessage from '../client/newModules/NotFound/NotFound'; not being used.
-// import { fetchComponentData } from './util/fetchData'; // eslint-disable-line no-unused-vars
 import dummyData from './dummyData';
 import serverConfig from './config';
 import challengeAttempt from './modules/challengeAttempt/routes';
@@ -139,100 +133,6 @@ export default function () {
       return next();
     });
   });
-
-  // ORIGINAL MERN IMPLEMENTATION.
-  // renders using REDUX
-  // Render Initial HTML
-/*
-  const renderFullPage = (html, initialState) => { // eslint-disable-line no-unused-vars
-    const head = Helmet.rewind();
-
-    // Import Manifests
-    const assetsManifest = process.env.webpackAssets && JSON.parse(process.env.webpackAssets);
-    const chunkManifest = process.env.webpackChunkAssets && JSON.parse(process.env.webpackChunkAssets);
-
-    return `
-      <!doctype html>
-      <html>
-        <head>
-          ${head.base.toString()}
-          ${head.title.toString()}
-          ${head.meta.toString()}
-          ${head.link.toString()}
-          ${head.script.toString()}
-
-          ${process.env.NODE_ENV === 'production' ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
-          <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
-          <link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
-        </head>
-        <body>
-          <div id="root">${html}</div>
-          <script>
-            window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
-            ${process.env.NODE_ENV === 'production' ?
-            `//<![CDATA[window.webpackManifest = ${JSON.stringify(chunkManifest)}
-            ;//]]>` : ''}
-          </script>
-          <script src='${process.env.NODE_ENV === 'production' ? assetsManifest['/vendor.js'] : '/vendor.js'}'></script>
-          <script src='${process.env.NODE_ENV === 'production' ? assetsManifest['/app.js'] : '/app.js'}'></script>
-        </body>
-      </html>
-    `;
-  };
-*/
-
-  // ORIGINAL MERN ERROR IMPLEMENTATION.
-  // it was change to to render newRenderFullPage with only one argument.
-/*
-  const renderError = err => {
-    const softTab = '&#32;&#32;&#32;&#32;';
-    const errTrace = process.env.NODE_ENV !== 'production' ?
-      `:<br><br><pre style="color:red">${softTab}${err.stack.replace(/\n/g, `<br>${softTab}`)}</pre>` : ''; // eslint-disable-line no-unused-vars
-    return renderFullPage(`Server Error${errTrace}`, {});
-  };
-*/
-
-  // ORIGINAL MERN IMPLEMENTATION. USING REDUX.
-  // Server Side Rendering based on routes matched by React-router.
-/*
-  app.use((req, res, next) => {
-    match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
-      console.log(`req.url is ${req.url}`); // eslint-disable-line no-console
-      if (err) {
-        return res.status(500).end(renderError(err));
-      }
-      if (redirectLocation) {
-        return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-      }
-
-      if (!renderProps) {
-        return next();
-      }
-
-      const store = configureStore();
-
-      return fetchComponentData(store, renderProps.components, renderProps.params)
-        .then(() => {
-          const initialView = renderToString(
-            <Provider store={store}>
-              <IntlWrapper>
-                <RouterContext {...renderProps} />
-              </IntlWrapper>
-            </Provider>
-          );
-          const finalState = store.getState();
-
-          res
-            .set('Content-Type', 'text/html')
-            .status(200)
-            .end(renderFullPage(initialView, finalState));
-        })
-        .catch((error) => next(error));
-    });
-  });
-*/
-
 
   app.use((req, res) => {
     return res.status(404).end('404');
